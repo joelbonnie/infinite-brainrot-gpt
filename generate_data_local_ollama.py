dd64a16a8d7e40b6c9b2d2f55a90090044795536
Joel Bonnie <joelbcasp@gmail.com>
2025-02-18 23:10:35 -0800

    wrote functions to process text, added processed txt file

--- generate_data_local_ollama.py
+++ generate_data_local_ollama.py
@@ -11,6 +11,7 @@ max_iters = 200
 brainrot_words = ""
 brainrot_text = ""
 brainrot_file = "brainrot.txt"
+processed_brainrot_file = "processed_brainrot.txt" 
 example_brainrot_words_1 = "slay, rizz, gyatt, sigma, skibidi, fanum tax, Ohio, NPC, edge, babygirl, lore, delulu, bussin, oomfie, chungus, sneaky link, bogged, glizzy, bala, grimy, ice, W, L, ewokie, mewing, vamp, baddie, zaza, situationship, iykyk, sigma grindset,"
 example_brainrot_words_2 = "Gyatt, Rizz, Skibidi, Sigma, Fanum Tax, Ohio, Edge, Babygirl, Bussin, NPC, Glazing, Lore, Maxxing, Chungus, Situationship, Delulu, Grindset, Pressing, High Key, Pookie, Mewing, UWU,"
 example_brainrot_words_3 = "skibidi, gyatt, rizz, fanum tax, oppenheimer style, sigma, lobotomy, kai cenat, grimace shake, delulu, glazing, leonardo dicaprio walking, mewing, NPC, ohio, skibidi toilet, gen alpha, pookie, ick, pick me, lore, eepy,bussin, no cap, cap, ate, slay, W, L, ratio, touch grass, bestie, main character, gatekeep, gaslight, girlboss, hot girl walk, it's giving, perioooodt, sheesh, sus, sending me, hits different, big yikes, lowkey, highkey,tea, spill, vibes, vibing, extra, lit, fire, snatched, ghosted, salty, gucci, GOAT, clout, FOMO, stan, simp, vibin, POV, on God, frfr, deadass, bet, drip,  hits different,  main character energy, it's giving, let him cook, let her cook, let them cook, be so fr, not them, sir/ma'am, iykyk, say sike rn, clowning, clownery, do it for the plot, the ick"
@@ -78,18 +79,33 @@ def generate_brainrot_text():
 
 
 
-
 ####################
 ## Write to file
 
 example_brainrot_words_list = example_brainrot_words_concat.split(",")
 
+def generate_text(): 
+    for i in range(max_iters):
+        print(f"Text Generation Iter: {i}")
+        brainrot_words = random.sample(example_brainrot_words_list, 8)
+        new_brainrot_text = "\n" + generate_brainrot_text()
+        with open(brainrot_file, "a") as fd: 
+                fd.write(new_brainrot_text)
 
-for i in range(max_iters):
-    print(f"Text Generation Iter: {i}")
-    brainrot_words = random.sample(example_brainrot_words_list, 8)
-    new_brainrot_text = "\n" + generate_brainrot_text()
-    with open(brainrot_file, "a") as fd: 
-            fd.write(new_brainrot_text)
+def process_text():
+    def remove_unwanted_chars(text):
+        cleaned_text = re.sub(r'\d+\.', '', text)
+        cleaned_text = re.sub(r'["\d]', '', cleaned_text)
+        cleaned_text = re.sub(r'[^\w\s.,!?]', '', cleaned_text)
+        return cleaned_text
 
+    with open(brainrot_file, "r", encoding="utf-8") as fd:
+        lines = fd.readlines()    
+
+    cleaned_lines = [remove_unwanted_chars(line) for line in lines]
+
+    with open(processed_brainrot_file, "w", encoding="utf-8") as fd:
+        fd.writelines(cleaned_lines)
+
+process_text()
 ####################
